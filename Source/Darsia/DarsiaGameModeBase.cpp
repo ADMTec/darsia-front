0b2d7ef161e4f1c3a96b121097f62d6af4ecbb6f
mateosss <mateodemayo@gmail.com>
2019-11-23 11:04:31 -0300

    Add many related changes to implement movement
    - Rename BP_GridMap to BP_Board
    - Add GetLocationFromCoord
    - Add Assert function and BPFL_Utils

--- Source/Darsia/DarsiaGameModeBase.cpp
+++ Source/Darsia/DarsiaGameModeBase.cpp
@@ -2,9 +2,12 @@
 #include "DarsiaCharacter.h"
 #include "GameFramework/Actor.h"
 #include "Engine/World.h"
+#include "Darsia.h"
 
 void ADarsiaGameModeBase::BeginPlay()
 {
+	Super::BeginPlay();
+	ulog("FARAFAAA");
 	//for (int i = 0; i < NumberOfPlayers; i++)
 	//{
 	//	Characters.Add(GetWorld()->SpawnActor<ADarsiaCharacter>(
